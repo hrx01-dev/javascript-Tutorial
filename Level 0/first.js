b0bb1a06daae3cf96594cf195bbe2d2d4d70a6f0
Jayam Srivastava <jayamsrivastava99@gmail.com>
2026-07-09 01:39:55 +0530

    Level0.1

--- Level 0/first.js
+++ Level 0/first.js
@@ -6,3 +6,16 @@ var x = 10
 console.log(x + 30)
 // Rules for variable
 // It should contain unique character , it treats "a"& "A" differently.
+// use camel case -firstName , lastName
+// Keywords 
+// var , const , let 
+// These are reserved words in JS 
+// var - can be redeclared and reinitialised
+// let - can be redeclared but not reinitialised
+// const - can neither be redeclared nor reinitialised
+let name = "jayam srivastava"
+// name = "j"  It can be updated within a variable unlike var that requires new var for updating .
+console.log(name)
+const PI = 3.14;
+console.log(PI)
+// let can be initialize later but not the const 
