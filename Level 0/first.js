41480d98d32b3ed5bf8a872a66c4e1873b5b1fca
Jayam Srivastava <jayamsrivastava99@gmail.com>
2026-07-09 01:20:20 +0530

    Level0

---
+++ Level 0/first.js
@@ -0,0 +1,8 @@
+// first JavaScript Program
+
+console.log("Hello , Jayam srivastava")
+// Variable declaration- these are containers that store values
+var x = 10
+console.log(x + 30)
+// Rules for variable
+// It should contain unique character , it treats "a"& "A" differently.
